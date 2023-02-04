87aeb5d22ce148703f2509924943fdf73c86b411
SumiAkter2 <akteralisha36@gmail.com>
2023-02-04 14:20:34 +0600

    fetching studentS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [students, setStudents] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/students")
+      .then(res => res.json())
+    .then(data=>setStudents(data))
+  }, [])
   return (
-    <div>
+    <div className="App">
       <h1>Simple node js for client site</h1>
+      {
+        students.length
+}
+      <form>
+        <input type="text" placeholder="Name" name="name" /><br />
+        <input type="text" placeholder="Fav Subject" name="favSub" /> <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
