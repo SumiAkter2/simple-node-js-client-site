312e171e9ddd04debc1f5a5c9a48578e890f61d5
SumiAkter2 <akteralisha36@gmail.com>
2023-02-04 14:25:37 +0600

    show students info from backend

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ function App() {
   const [students, setStudents] = useState([]);
   useEffect(() => {
     fetch("http://localhost:5000/students")
-      .then(res => res.json())
-    .then(data=>setStudents(data))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setStudents(data));
+  }, []);
   return (
     <div className="App">
       <h1>Simple node js for client site</h1>
-      {
-        students.length
-}
+      <h2>{students.length}</h2>
+      {students.map((student) => (
+        <ul style={{ listStyleType: "square" }}>
+          <li key={student.id}>
+            {student.id} -----------{student.name}----------
+            {student.favSub}
+          </li>
+        </ul>
+      ))}
       <form>
-        <input type="text" placeholder="Name" name="name" /><br />
+        <input type="text" placeholder="Name" name="name" />
+        <br />
         <input type="text" placeholder="Fav Subject" name="favSub" /> <br />
         <button>Submit</button>
       </form>
